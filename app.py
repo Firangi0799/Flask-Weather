da2c61efc2f3fb93f92c15400168020071f3ee0b
Abhishek Kumar <88920745+Firangi0799@users.noreply.github.com>
2023-07-08 12:39:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,6 @@ import pytz
 from geopy.geocoders import Photon
 from timezonefinder import TimezoneFinder
 from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///city_searches.db'
-db = SQLAlchemy(app)
-
-class City(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return f"City('{self.name}', '{self.timestamp}')"
 
 @app.route("/")
 def index():
